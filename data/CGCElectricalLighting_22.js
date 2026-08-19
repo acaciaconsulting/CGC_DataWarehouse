48c57aef614ecb24da55a54fec168ab49b3f3760
acaciaconsulting <david.williamson@iinet.net.au>
2026-08-19 15:41:58 +1000

    2026-08 CGC Range Update
    2026-08 CGC Range Update

--- data/CGCElectricalLighting_22.js
+++
@@ -1,1 +0,0 @@
-var json_CGCElectricalLighting_22 = {"type":"FeatureCollection","name":"CGCElectricalLighting_22","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
